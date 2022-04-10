074331ae61a4bcff70c908582a4b4ed320031950
indexpz <pawelz72@gmail.com>
2022-04-10 18:38:58 +0200

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from "./Form";
+import Image from './Image'
+
+export default function Main() {
+    return (<>
+            <p>Main</p>
+            <ul>
+                <li><Form/></li>
+                <li><Image/></li>
+            </ul>
+
+        </>
+    )
+}
